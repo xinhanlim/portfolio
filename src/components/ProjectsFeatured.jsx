bf9d62bf86889e72210b4b52de02ea6ad3e142d3
xinhanlim <xhxhan00@gmail.com>
2025-08-02 10:25:49 +0000

    added a hr

--- src/components/ProjectsFeatured.jsx
+++ src/components/ProjectsFeatured.jsx
@@ -4,8 +4,8 @@ import React from 'react';
 export default function ProjectFeature() {
     return (<>
 
-        <div className='bg-[#282828] flex quicksand-headerText justify-between w-full px-8 py-36 h-screen'>
-            
+        <div className='bg-[#F2F0EF] flex quicksand-headerText justify-between w-full px-8 h-screen'>
+            <hr className="w-full"></hr>
         </div>
     </>
 
