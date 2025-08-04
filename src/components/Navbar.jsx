2a7a07399182d0c9cbf9f79623cfdbd00a6c788f
xinhanlim <xhxhan00@gmail.com>
2025-08-04 13:25:33 +0000

    added corret spacing for grid

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,14 +4,14 @@ import React from "react";
 export default function Navbar() {
     return (<>
         <nav className="fixed z-50 quicksand-navBarText flex justify-center w-full transition duration-300  bg-[#F2F0EF]/40 backdrop-blur-xl ">
-            <div className="flex justify-between items-center w-full px-8 py-6  mx-auto ">
+            <div className="flex justify-between items-center w-full px-8 py-6  mx-auto max-w-[1440px] ">
                 <div>
-                    <h1 className="quicksand-navBarText600 flex flex-1 text-xl">XHLIM</h1>
+                    <a href="/" className="quicksand-navBarText600 hover:transition-all duration-300">XH:LIM</a>
                 </div>
                 <div>
                     <ul className="flex text-md gap-10 items-center w-full">
                         <li>
-                            <a href="/" className="hover:underline transition-all duration-300">PROJECT</a>
+                            <a href="/" className="hover:underline transition-all duration-300">WORK</a>
                         </li>
                         <li>
                             <a href="/" className="hover:underline transition-all duration-300" >ABOUT</a>
--- src/components/ProjectsFeatured.jsx
+++ src/components/ProjectsFeatured.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 export default function ProjectFeature() {
     return (<>
 
-        <div className="max-w-[1440px] mx-auto grid grid-cols-1 gap-8 quicksand-headerText sm:grid-cols-2 pb-[64px] md:px-4 lg:px-0 sm:px-4">
+        <div className="max-w-[1440px] mx-auto grid grid-cols-1 gap-8 quicksand-headerText px-8 sm:grid-cols-2 pb-[64px] md:px-4 lg:px-8">
 
             <img src="https://picsum.photos/200/300" alt="project1" className="w-full object-cover rounded-lg max-h-[700px] border-1 border-[#282828]/20 shadow-sm" />
             <img src="https://picsum.photos/200/300" alt="project1" className="w-full object-cover rounded-lg max-h-[700px] border-1 border-[#282828]/20 shadow-sm" />
