f23dbd5e6e1125601964ffdcfc55e87e5fcd8a0e
xinhanlim <xhxhan00@gmail.com>
2025-09-29 08:13:19 +0000

    updated header name

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -8,16 +8,19 @@ export default function ProjectCard() {
         {projectDetails.map((project) => (
             <figure key={project.id}>
                 <Link href={`/project/${project.id}`} >
-                    <motion.div initial={{ y: -40 , opacity: 0 }}
+                    <motion.div initial={{ y: -40, opacity: 0 }}
                         animate={{ y: 0, opacity: 1 }}
                         transition={{ delay: 0.7, duration: 0.9 }} className="group relative  overflow-hidden rounded-md border border-[#282828]/20">
                         <img src={project.image} alt={project.name} className="w-full object-cover rounded-lg max-h-[700px] shadow-sm transform transition duration-300 ease-in-out hover:scale-103" />
                     </motion.div>
                 </Link>
                 <figcaption className="flex items-center quicksand-bodyText gap-8 py-4 ">
-                    <a href={project.livelink} className="text-md text-bold tracking-tighter hover:underline underline-offset-4">Live Demo</a>
-                    <a href={project.githubfe} className="text-md text-bold tracking-tighter hover:underline underline-offset-4">Front End</a>
-                    <a href={project.githubfe} className="text-md text-bold tracking-tighter hover:underline underline-offset-4">Back End</a>
+                    <a href={project.livelink} target="_blank"
+                        rel="noopener noreferrer" className="text-md text-bold tracking-tighter hover:underline underline-offset-4">Live Demo</a>
+                    <a href={project.githubfe} target="_blank"
+                        rel="noopener noreferrer" className="text-md text-bold tracking-tighter hover:underline underline-offset-4">Front End</a>
+                    <a href={project.githubfe} target="_blank"
+                        rel="noopener noreferrer" className="text-md text-bold tracking-tighter hover:underline underline-offset-4">Back End</a>
                 </figcaption>
             </figure>
         ))}
